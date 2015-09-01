3052c6d612ea88dc37514968da618fa9d9079fb8
HarleyVanselow <code@harleyv.net>
2015-08-31 20:49:17 -0600

    All files

---
+++ PolyRelativity/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+using System.Threading;
+
+
+
+namespace PolyRelativity
+{
+  public enum EColor { Red, Green, Blue };
+  public partial class Form1 : Form
+  {
+    public static CDrawer canvas = new CDrawer(1000, 1000);
+    public EColor c = EColor.Red;
+    public static Random rand = new Random();
+    static List<Shape> _shapes = new List<Shape>();
+    public Form1()
+    {
+      InitializeComponent();
+      //canvas.Scale = 5;
+
+
+      // center solid anchors
+      _shapes.Add(new FixedSquare(new Point(450, 500), Color.Red, null));
+      _shapes.Add(new FixedSquare(new Point(550, 500), Color.Red, _shapes[0]));
+      // ccw orbit chain
+      {
+        List<Shape> local = new List<Shape>();
+        local.Add(new OrbitBall(Color.Yellow, 50, _shapes[0], -0.1, Math.PI));
+        local.Add(new OrbitBall(Color.Pink, 50, local[0], -0.15, Math.PI));
+        local.Add(new OrbitBall(Color.Blue, 50, local[1], -0.2, Math.PI));
+        local.Add(new OrbitBall(Color.Green, 50, local[2], -0.25, Math.PI));
+        _shapes.AddRange(local);
+      }
+      // cw orbit chain
+      {
+        List<Shape> local = new List<Shape>();
+        local.Add(new OrbitBall(Color.Yellow, 50, _shapes[1], 0.05));
+        local.Add(new OrbitBall(Color.Pink, 50, local[0], 0.075));
+        local.Add(new OrbitBall(Color.Blue, 50, local[1], 0.1));
+        local.Add(new OrbitBall(Color.Green, 50, local[2], 0.125));
+        _shapes.AddRange(local);
+      }
+      // fixed/double h/v wobble + orbit chain
+      {
+        List<Shape> local = new List<Shape>();
+        local.Add(new FixedSquare(new Point(200, 500), Color.Cyan, null));
+        local.Add(new VWobbleBall(Color.Red, 100, local[0], 0.1));
+        local.Add(new HWobbleBall(Color.Red, 100, local[1], 0.15));
+        local.Add(new OrbitBall(Color.LightBlue, 25, local[2], 0.2));
+        _shapes.AddRange(local);
+      }
+      // show the top row of solid blocks with incremental offset animated vwballs
+      {
+        List<Shape> localA = new List<Shape>();
+        List<Shape> localB = new List<Shape>();
+        for (int i = 50; i < 1000; i += 50)
+          localA.Add(new FixedSquare(new Point(i, 100), Color.Cyan, null));
+        _shapes.AddRange(localA);
+        double so = 0;
+        foreach (Shape s in localA)
+        {
+          localB.Add(new VWobbleBall(Color.Purple, 50, s, 0.1, so += 0.7));
+
+        }
+        _shapes.AddRange(localB);
+      }
+
+      // show 3-tier cloud of quad balls orbiting the same block
+      {
+        List<Shape> local = new List<Shape>();
+        local.Add(new FixedSquare(new Point(800, 500), Color.GreenYellow, null));
+        local.Add(new OrbitBall(Color.Yellow, 30, local[0], 0.1, 0));
+        local.Add(new OrbitBall(Color.Yellow, 30, local[0], 0.1, Math.PI / 2));
+        local.Add(new OrbitBall(Color.Yellow, 30, local[0], 0.1, Math.PI));
+        local.Add(new OrbitBall(Color.Yellow, 30, local[0], 0.1, 3 * Math.PI / 2));
+        local.Add(new OrbitBall(Color.Yellow, 60, local[0], -0.05, 0));
+        local.Add(new OrbitBall(Color.Yellow, 60, local[0], -0.05, Math.PI / 2));
+        local.Add(new OrbitBall(Color.Yellow, 60, local[0], -0.05, 3 * Math.PI));
+        local.Add(new OrbitBall(Color.Yellow, 60, local[0], -0.05, 3 * Math.PI / 2));
+        local.Add(new OrbitBall(Color.Yellow, 90, local[0], 0.025, 0));
+        local.Add(new OrbitBall(Color.Yellow, 90, local[0], 0.025, Math.PI / 2));
+        local.Add(new OrbitBall(Color.Yellow, 90, local[0], 0.025, Math.PI));
+        local.Add(new OrbitBall(Color.Yellow, 90, local[0], 0.025, 3 * Math.PI / 2));
+        _shapes.AddRange(local);
+      }
+      { // show OrbitBall with ratio value, elliptical orbits
+        List<Shape> local = new List<Shape>();
+        local.Add(new FixedSquare(new Point(500, 400), Color.Violet, null));
+        local.Add(new OrbitBall(Color.Yellow, 200, local[0], 0.1, 0, 2));
+        local.Add(new OrbitBall(Color.Yellow, 200, local[0], 0.1, Math.PI / 2, 2));
+        local.Add(new OrbitBall(Color.Yellow, 200, local[0], 0.1, Math.PI, 2));
+        local.Add(new OrbitBall(Color.Yellow, 200, local[0], 0.1, 3 * Math.PI / 2, 2));
+        local.Add(new OrbitBall(Color.Yellow, 200, local[0], -0.05, 0, 0.5));
+        local.Add(new OrbitBall(Color.Yellow, 200, local[0], -0.05, Math.PI / 2, 0.5));
+        local.Add(new OrbitBall(Color.Yellow, 200, local[0], -0.05, 3 * Math.PI, 0.5));
+        local.Add(new OrbitBall(Color.Yellow, 200, local[0], -0.05, 3 * Math.PI / 2, 0.5));
+        _shapes.AddRange(local);
+      }
+      // show animated polygons (interlocking triangles)
+      _shapes.Add(new AniPoly(new Point(100, 300), Color.Tomato, 3, null, 0.1));
+      _shapes.Add(new AniPoly(new Point(135, 300), Color.Tomato, 3, null, -0.1, 1));
+      _shapes.Add(new AniPoly(new Point(170, 300), Color.Tomato, 3, null, 0.1));
+      // show string of adjacent relative horizontal wobble balls
+      {
+        List<Shape> local = new List<Shape>();
+        local.Add(new FixedSquare(new Point(500, 200), Color.Wheat, null));
+        for (int i = 1; i < 20; ++i)
+          local.Add(new HWobbleBall(Color.Orange, 25, local[i - 1], 0.1));
+        _shapes.AddRange(local);
+      }
+      // show highlight on a fixed square
+      {
+        List<Shape> local = new List<Shape>();
+        local.Add(new FixedSquare(new Point(800, 300), Color.LightCoral, null));
+        local.Add(new AniHighlight(Color.Yellow, 30, local[0], 1));
+        _shapes.AddRange(local);
+      }
+
+
+
+    }
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      canvas.Clear();
+      foreach (Shape s in _shapes)
+      {
+
+        if (s is AniShape)
+        {
+          AniShape ani_s = (AniShape)s;
+          ani_s.Tick();
+        }
+        s.Render(canvas);
+      }
+
+      canvas.Render();
+    }
+
+
+
+
+
+
+
+
+  }
+}
---
+++ PolyRelativity/Shapes.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDIDrawer;
+using System.Drawing;
+
+namespace PolyRelativity
+{
+  public interface IRender
+  {
+    void Render(CDrawer dr);
+  }
+  public interface IAnimate
+  {
+    void Tick();
+  }
+  public abstract class Shape:IRender
+  {
+    protected Point position;
+    public Point Position
+    {
+      get { return position; }
+    }
+    protected Color color;
+    protected Shape parent;
+
+    protected int size;
+    public int Size
+    {
+      get { return size; }
+    }
+    public Shape(Point p, Color c,Shape parent=null)
+    {
+      this.position = p;
+      this.color = c;
+      this.parent = parent;
+    }
+    protected abstract void vRender(CDrawer dr);
+    public void Render(CDrawer dr)
+    {
+      if(parent!=null)dr.AddLine(position.X, position.Y, parent.position.X, parent.position.Y, Color.White);
+      vRender(dr);
+    }
+
+
+  }
+  public abstract class AniShape : Shape, IAnimate
+  {
+    protected double sequence_val;
+    protected double sequence_delta;
+    public AniShape(Point p, Color c,double sv,double sd)
+      : base(p, c)
+    {
+      this.sequence_delta = sd;
+      this.sequence_val = sv;
+    }
+    public abstract void vTick();
+
+    public void Tick()
+    {
+      vTick();
+    }
+  }
+  class FixedSquare : Shape
+  {
+    public FixedSquare(Point p,Color c,Shape parent=null)
+      : base(p,c,parent)
+    {
+      this.size = 20;
+    }
+    protected override void vRender(CDrawer canvas)
+    {
+      canvas.AddRectangle(position.X - 10, position.Y - 10, size, size, color);
+    }
+  }
+  public class AniPoly : AniShape
+  {
+    private int sides;
+    public AniPoly(Point p, Color c,int sides,Shape parent,double sd,double sv=0)
+      : base(p, c,sv,sd)
+    {
+      if (sides >= 3) this.sides = sides;
+      else throw new ArgumentException("Shape must have at least 3 sides");
+      this.size = 25;
+    }
+    protected override void vRender(CDrawer dr)
+    {
+      dr.AddPolygon(position.X, position.Y, size, sides, sequence_val, color);
+    }
+    public override void vTick()
+    {
+      this.sequence_val += this.sequence_delta;
+    }
+  }
+  public abstract class AniChild : AniShape
+  {
+    protected double distance;
+    public AniChild(Point p, Color c, Shape parent,double sd,double sv,double distance)
+      : base(p, c,sv,sd)
+    {
+      this.distance = distance;
+      if (parent != null) this.parent = parent;
+      else throw new ArgumentException("Must supply a non-null parent");
+      Tick();
+    }
+  }
+  public abstract class AniBall : AniChild
+  {
+    public AniBall(Point p, Color c, Shape parent, double sd, double sv, double distance)
+      : base(p, c, parent,sd,sv,distance)
+    {
+      this.size = 20;
+    }
+    protected override void vRender(CDrawer dr)
+    {
+      dr.AddCenteredEllipse(position.X, position.Y, size, size, color);
+    }
+  }
+  class AniHighlight : AniChild
+  {
+    private int temp_size;
+    private bool grow_direction = true;
+    public AniHighlight(Color c, double distance, Shape parent, double sd)
+      :base(new Point(0,0),c,parent,sd,0,distance)
+    {
+      this.size = parent.Size;
+      this.temp_size = size;
+      this.position = parent.Position;
+    }
+    public override void vTick()
+    {
+      if (temp_size + sequence_delta >= distance && grow_direction)
+      {
+        grow_direction=false;
+      }
+      else if (!grow_direction && temp_size - sequence_delta < size)
+      {
+        grow_direction = true;
+      }
+      temp_size = grow_direction ? temp_size + (int)sequence_delta : temp_size - (int)sequence_delta;
+
+
+    }
+    protected override void vRender(CDrawer dr)
+    {
+      dr.AddCenteredEllipse(position.X, position.Y, temp_size, temp_size, Color.White);
+    }
+  }
+  class OrbitBall : AniBall
+  {
+    private double ratio;
+    public OrbitBall(Color c,double distance, Shape parent, double sd ,double sv=0,double ratio=1)
+      : base(new Point(0,0), c, parent, sd, sv, distance)
+    {
+      this.ratio = ratio;
+    }
+    public override void vTick()
+    {
+      this.position.Y = parent.Position.Y+(int)(Math.Sin(sequence_val)*distance*ratio);
+      this.position.X = parent.Position.X + (int)(Math.Cos(sequence_val) * distance);
+      sequence_val += sequence_delta;
+    }
+  }
+  class VWobbleBall : AniBall
+  {
+    public VWobbleBall(Color c, double distance, Shape parent, double sd,double sv=0)
+      : base(new Point(0, 0), c, parent, sd, sv, distance)
+    {
+      position.X = parent.Position.X;
+    }
+    public override void vTick()
+    {
+      position.Y = parent.Position.Y + (int)(Math.Cos(sequence_val) *distance);
+      sequence_val += sequence_delta;
+    }
+  }
+  class HWobbleBall : AniBall
+  {
+    public HWobbleBall(Color c, double distance, Shape parent, double sd,double sv=0)
+      : base(new Point(0, 0), c, parent, sd, sv, distance)
+    {
+      position.Y = parent.Position.Y;
+    }
+    public override void vTick()
+    {
+      position.X = parent.Position.X + (int)(Math.Cos(sequence_val) *distance);
+      
+      sequence_val += sequence_delta;
+    }
+  }
+  
+}
+
